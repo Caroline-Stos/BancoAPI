d7e58cca76e8c0d669a78062806a5c79f446eef3
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-08-26 22:02:48 -0300

    Adicionar arquivos de projeto.

---
+++ CaixaEletronico/Program.cs
@@ -0,0 +1,16 @@
+﻿using Conta;
+using System.Net;
+
+ContaFisica Pessoa = new ContaFisica();
+
+Console.WriteLine(Pessoa.VerSaldo());
+
+Console.WriteLine(Pessoa.Depositar(15));
+
+Console.WriteLine(Pessoa.Sacar(16));
+
+Pessoa.Nome = "Caroline";
+Pessoa.CPF = 12345678;
+Pessoa.Idade = 26;
+Pessoa.Profissao = "Desenvolvedora Junior";
+Pessoa.Endereco = "Avenida Rio Pequeno, 118";
---
+++ Pessoa/IConta.cs
@@ -0,0 +1,17 @@
+﻿namespace Conta
+{
+    public interface IConta
+    {
+        // propriedades
+        int NumAgencia { get; set; }
+        int NumConta { get; set; }
+        string TipoConta { get; set; }
+        double Saldo { get; set; }
+        string Endereco { get; set; }
+
+        // metodos
+        string Sacar(double valor);
+        string Depositar(double valor);
+        string VerSaldo();
+    }
+}
