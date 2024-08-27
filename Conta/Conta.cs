23bc83d96edea279fc7d058f56738e32f73d9507
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-08-26 22:04:13 -0300

    renomeando pasta

---
+++ Conta/Conta.cs
@@ -0,0 +1,43 @@
+﻿namespace Conta 
+{
+    public class Conta : IConta
+    {
+        private int _saldo;
+        public int NumAgencia { get; set; } = 0;
+        public int NumConta { get; set; } = 0;
+        public string TipoConta { get; set; } = string.Empty;
+        public double Saldo { get; set; } = 0;
+        public string Endereco {  get; set; } = string.Empty;
+
+        //metodos
+        public virtual string Sacar(double valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+                return $"Saque concluído no valor de R${valor}";
+            }
+            else
+            {
+                return "Saldo insuficiente";
+            }
+        }
+
+        public virtual string Depositar(double valor)
+        {
+            if (double.IsPositive(valor))
+            {
+                Saldo += valor;
+                return $"Depósito realizado com sucesso no valor de R${Saldo}";
+            }
+            else
+            {
+                return "Valor de deposito inválido";
+            }
+        }
+        public virtual string VerSaldo()
+        {
+            return $"Saldo: R${Saldo} disponível";
+        }
+    }
+}
---
+++ Conta/ContaJuridica.cs
@@ -0,0 +1,26 @@
+﻿namespace Conta
+{
+    public class ContaJuridica : Conta
+    {
+        public string NomeFantasia { get; set; } 
+        private int CNPJ { get; set; } = 0;
+        public string Socio_1 { get; set; }
+        public string Socio_2 { get; set; }
+        public int CPF_Socio_1 { get; set; }
+        public int CPF_Socio_2 { get; set; }
+
+        // metodos 
+        public override string Sacar(double valor)
+        {
+            return base.Sacar(valor);
+        }
+        public override string Depositar(double valor)
+        {
+            return base.Depositar(valor);
+        }
+        public override string VerSaldo()
+        {
+            return base.VerSaldo();
+        }
+    }
+}
